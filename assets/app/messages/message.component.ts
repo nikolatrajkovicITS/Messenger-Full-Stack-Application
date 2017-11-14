28260eba34534ec877129bd2a7f7528587a4a6f4
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-11-14 12:08:41 +0100

    Created models, improving the Folder structure and create Message component.

---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,24 @@
+import { selector } from 'rxjs/operator/publish';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-message',
+    templateUrl: './message.component.html',
+    styles: ['
+    .author {
+        display: inline-block;
+        font-style: italic;
+        font-size: 12px;
+        width: 80%;
+    }
+    .config {
+      display: inline-block;
+      text-align: right;
+      text-size: 12px;
+      width: 19%;
+    }
+  ']
+})
+export class MessageComponent {
+
+}
